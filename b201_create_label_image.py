41b3ef447e9e685452ca1d3c00f18bcdbed07aa8
sunabove <sunabove@users.noreply.github.com>
2022-01-20 16:16:43 +0900

    **

--- b201_create_label_image.py
+++ b201_create_label_image.py
@@ -1,6 +1,6 @@
 import os, json, cv2, cv2 as cv
 
-def create_label_image( json_file_path, image_folder, image_label_folder, debug=0 ) :
+def create_label_image( json_file_path, image_folder, image_label_folder, colors=None, debug=0 ) :
     image_label_path = None
     with open( json_file_path, 'r') as f:
         json_data = json.load( f )
@@ -19,12 +19,18 @@ def create_label_image( json_file_path, image_folder, image_label_folder, debug=
             debug and print( f"image_path = {image_path}" )
             debug and print( f"image_label_path = {image_label_path}" )
 
-            image = cv2.imread( image_path )
+            image = cv2.imread( image_path ) 
 
-            annotations =  label_info['annotations']
-            for annot in annotations:
-                box = annot['bbox'] # 객체 1개의 bbox 위치 좌표
-                cv2.rectangle(image, box[0:2], box[2:], (255,0,0), 2 )
+            if colors is None :
+                colors = [ (255,0,0), (0, 255, 0), (0, 0, 255), (0, 255, 255), (255, 255, 0), (255, 0, 255) ]
+            pass
+
+            col_len = len( colors )
+            for idx, anno in enumerate( label_info['annotations'] ):
+                box = anno['bbox'] # 객체 1개의 bbox 위치 좌표
+                idx = idx%col_len
+                width = 2 if idx else 4
+                cv2.rectangle(image, box[0:2], box[2:], colors[ idx ], width )
             pass
 
             cv2.imwrite( image_label_path, image ) 
