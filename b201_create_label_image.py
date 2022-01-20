1f93505728f670ee0014fa755d95873931516f71
sunabove <sunabove@users.noreply.github.com>
2022-01-20 16:05:42 +0900

    **

--- b201_create_label_image.py
+++ b201_create_label_image.py
@@ -1,8 +1,8 @@
 import os, json, cv2, cv2 as cv
 
-def create_label_image( json_file_name, image_folder, image_label_folder ) :
+def create_label_image( json_file_path, image_folder, image_label_folder, debug=0 ) :
     image_label_path = None
-    with open( json_file_name, 'r') as f:
+    with open( json_file_path, 'r') as f:
         json_data = json.load( f )
 
         label_info = json_data['label_info']
@@ -12,9 +12,13 @@ def create_label_image( json_file_name, image_folder, image_label_folder ) :
         image_path = image_folder + image_file_name 
 
         if not os.path.exists( image_path ) : 
-            print( f"image file [{image_file_name}] does not exists." )
+            debug and print( f"image file [{image_file_name}] does not exists." )
         else : 
-            print( f"image_file_name = {image_file_name}")
+            image_label_path = image_label_folder + image_file_name.replace( ".jpg", "_label.jpg" )
+            
+            debug and print( f"image_path = {image_path}" )
+            debug and print( f"image_label_path = {image_label_path}" )
+
             image = cv2.imread( image_path )
 
             annotations =  label_info['annotations']
@@ -23,10 +27,6 @@ def create_label_image( json_file_name, image_folder, image_label_folder ) :
                 cv2.rectangle(image, box[0:2], box[2:], (255,0,0), 2 )
             pass
 
-            print( f"image_path = {image_path}" )
-
-            image_label_path = image_label_folder + image_file_name.replace( ".jpg", "_label.jpg" )
-            print( f"image_label_path = {image_label_path}" )
             cv2.imwrite( image_label_path, image ) 
         pass
     pass
